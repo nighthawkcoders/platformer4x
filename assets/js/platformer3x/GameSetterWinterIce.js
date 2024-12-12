3312c69c225157fca2e113bd9070b13b131e159c
zoe-coding123 <zoechow44@gmail.com>
2024-12-12 02:32:06 -0800

    Changing penguin image

--- assets/js/platformer3x/GameSetterWinterIce.js
+++ assets/js/platformer3x/GameSetterWinterIce.js
@@ -336,8 +336,8 @@ const assets = {
     },
     Penguin: {
       src: "/images/platformer/sprites/penguin.png",
-      width: 477,
-      height: 477,
+      width: 385,
+      height: 420,
       scaleSize: 80,
       speedRatio: 0.6,
       xPercentage: 0.6,
