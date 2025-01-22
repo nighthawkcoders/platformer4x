9173069e5a1f81a6344ba0f4af28aebcced92dbc
Prahalad-ship-it <srinp131@gmail.com>
2025-01-22 14:26:51 -0800

    update

--- assets/js/platformer/GameSetterWater.js
+++ assets/js/platformer/GameSetterWater.js
@@ -16,99 +16,99 @@ import FinishLine from './FinishLine.js';
 // Define the GameSetup object literal
 const assets = {  
     obstacles: {
-      chest: {
-        src: "/images/platformer/obstacles/Chest.png",
-        hitbox: { widthPercentage: 0.5, heightPercentage: 0.5 },
-        width: 300,
-        height: 300,
-        scaleSize: 150,
-      },
-      coin: { src: "/images/platformer/obstacles/coin.png" },
-      dimonds: { src: "/images/platformer/obstacles/dimonds.png" },
+        chest: {
+            src: "/images/platformer/obstacles/Chest.png",
+            hitbox: { widthPercentage: 0.5, heightPercentage: 0.5 },
+            width: 300,
+            height: 300,
+            scaleSize: 150,
+        },
+        coin: { src: "/images/platformer/obstacles/coin.png" },
+        dimonds: { src: "/images/platformer/obstacles/dimonds.png" },
     },
     platforms: {
-      sand: { src: "/images/platformer/platforms/sand.png" },
-      sandblock: {src:"/images/platformer/platforms/sandblock.png"},
-      block: { src: "/images/platformer/platforms/brick_block.png" }, 
-      itemBlock: {
-        src: "/images/platformer/platforms/mario_block_spritesheet_v2.png",
-        sizeRatio: 83.2,
-        widthRatio: 0.5,
-        heightRatio: 1.0,
-        width: 204,
-        height: 204,
-        scaleSize: 80,
-        speedRatio: 0.7,
-        hitbox: { widthPercentage: 0.4, heightPercentage: -0.2 }
-      }
+        sand: { src: "/images/platformer/platforms/sand.png" },
+        sandblock: { src: "/images/platformer/platforms/sandblock.png" },
+        block: { src: "/images/platformer/platforms/brick_block.png" }, 
+        itemBlock: {
+            src: "/images/platformer/platforms/mario_block_spritesheet_v2.png",
+            sizeRatio: 83.2,
+            widthRatio: 0.5,
+            heightRatio: 1.0,
+            width: 204,
+            height: 204,
+            scaleSize: 80,
+            speedRatio: 0.7,
+            hitbox: { widthPercentage: 0.4, heightPercentage: -0.2 }
+        }
     },
     backgrounds: {
-      water: { src: "/images/platformer/backgrounds/water.png" },
-      fish: { src: "/images/platformer/backgrounds/school-fish.png", parallaxSpeed: -0.5 },
-      reef: { src: "/images/platformer/backgrounds/reef.png" },
+        water: { src: "/images/platformer/backgrounds/water.png" },
+        fish: { src: "/images/platformer/backgrounds/school-fish.png", parallaxSpeed: -0.5 },
+        reef: { src: "/images/platformer/backgrounds/reef.png" },
     },
     transitions: {
-      loading: { src: "/images/platformer/transitions/greenscreen.png" },
-      waterEnd: { src: "/images/platformer/transitions/waterEnd.png" },
-      miniEnd: { src: "/images/platformer/transitions/miniEnd.png" },
+        loading: { src: "/images/platformer/transitions/greenscreen.png" },
+        waterEnd: { src: "/images/platformer/transitions/waterEnd.png" },
+        miniEnd: { src: "/images/platformer/transitions/miniEnd.png" },
     },
     players: {
-      mario: {
-        src: "/images/platformer/sprites/mario.png",
-        width: 256,
-        height: 256,
-        scaleSize: 80,
-        speedRatio: 0.7,
-        idle: {
-          left: { row: 1, frames: 15 },
-          right: { row: 0, frames: 15 },
+        mario: {
+            src: "/images/platformer/sprites/mario.png",
+            width: 256,
+            height: 256,
+            scaleSize: 80,
+            speedRatio: 0.7,
+            idle: {
+                left: { row: 1, frames: 15 },
+                right: { row: 0, frames: 15 },
+            },
+            walk: {
+                left: { row: 3, frames: 7 },
+                right: { row: 2, frames: 7 },
+            },
+            run: {
+                left: { row: 5, frames: 15 },
+                right: { row: 4, frames: 15 },
+            },
+            jump: {
+                left: { row: 11, frames: 15 },
+                right: { row: 10, frames: 15 },
+            },
+            hitbox: { widthPercentage: 0.3, heightPercentage: 0.8 }
         },
-        walk: {
-          left: { row: 3, frames: 7 },
-          right: { row: 2, frames: 7 },
-        },
-        run: {
-          left: { row: 5, frames: 15 },
-          right: { row: 4, frames: 15 },
-        },
-        jump: {
-          left: { row: 11, frames: 15 },
-          right: { row: 10, frames: 15 },
-        },
-        hitbox: { widthPercentage: 0.3, heightPercentage: 0.8 }
-      },
     },
     enemies: {
-      goomba: {
-        src: "/images/platformer/sprites/Fish.png",
-        width: 448,
-        height: 452,
-        scaleSize: 60,
-        speedRatio: 0.7,
-        xPercentage: 0.6,
-        hitbox: { widthPercentage: 0.0, heightPercentage: 0.2 }
-      },
-      flyingGoomba: {
-        src: "/images/platformer/sprites/fancyturtle.png",
-        width: 448,
-        height: 452,
-        scaleSize: 60,
-        speedRatio: 0.7,
-      },
-      mushroom: {
-        src: "/images/platformer/platforms/mushroom.png",
-        width: 200,
-        height: 180,
-        hitbox: { widthPercentage: 0.0, heightPercentage: 0.2 }
-      },
-      bubbles: {
-        src: "/images/platformer/platforms/bubbles.png",
-        width: 200,
-        height: 180,
-        hitbox: { widthPercentage: 0.0, heightPercentage: 0.2 }
-      },
+        goomba: {
+            src: "/images/platformer/sprites/Fish.png",
+            width: 448,
+            height: 452,
+            scaleSize: 60,
+            speedRatio: 0.7,
+            xPercentage: 0.6,
+            hitbox: { widthPercentage: 0.0, heightPercentage: 0.2 }
+        },
+        flyingGoomba: {
+            src: "/images/platformer/sprites/fancyturtle.png",
+            width: 448,
+            height: 452,
+            scaleSize: 60,
+            speedRatio: 0.7,
+        },
+        mushroom: {
+            src: "/images/platformer/platforms/mushroom.png",
+            width: 200,
+            height: 180,
+            hitbox: { widthPercentage: 0.0, heightPercentage: 0.2 }
+        },
+        bubbles: {
+            src: "/images/platformer/platforms/bubbles.png",
+            width: 200,
+            height: 180,
+            hitbox: { widthPercentage: 0.0, heightPercentage: 0.2 }
+        },
     }
-  };
+};
 
 const objects = [
     { name: 'water', id: 'background', class: Background, data: assets.backgrounds.water },
